e0cab88fd7adc55cfc95313524fdcea24b7e8eff
Andrii Syrokomskyi <andrii@syrokomskyi.com>
2026-01-10 21:58:26 +0100

    Initial commit

---
+++ quartz/plugins/transformers/syntax.ts
@@ -0,0 +1,32 @@
+import rehypePrettyCode, { Options as CodeOptions, Theme as CodeTheme } from "rehype-pretty-code";
+
+import { QuartzTransformerPlugin } from "../types";
+
+interface Theme extends Record<string, CodeTheme> {
+  light: CodeTheme
+  dark: CodeTheme
+}
+
+interface Options {
+  theme?: Theme
+  keepBackground?: boolean
+}
+
+const defaultOptions: Options = {
+	theme: {
+		light: "github-light",
+		dark: "github-dark",
+	},
+	keepBackground: false,
+};
+
+export const SyntaxHighlighting: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
+  const opts: CodeOptions = { ...defaultOptions, ...userOpts }
+
+	return {
+		name: "SyntaxHighlighting",
+		htmlPlugins() {
+			return [[rehypePrettyCode, opts]];
+		},
+	};
+};
