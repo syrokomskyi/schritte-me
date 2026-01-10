cb8f7788f879a7b02d7b9a534b1593ce802d11be
Andrii Syrokomskyi <signmotion@gmail.com>
2026-01-11 00:14:28 +0100

    Change locale from en-US to de-DE.

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "de-DE",
     baseUrl: "schritte.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
